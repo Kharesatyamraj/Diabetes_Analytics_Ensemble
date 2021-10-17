27d8a8e25f1e55a34a380a654874a210232baeb3
Satyam Raj Khare <89503508+Kharesatyamraj@users.noreply.github.com>
2021-10-17 19:58:11 +0530

    Add files via upload

---
+++ Diabetes.py
@@ -0,0 +1,304 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 17 19:47:49 2021
+
+@author: khare
+"""
+
+
+## Name- SATYAM RAJ KHARE
+
+
+
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#1.	Given is the diabetes dataset. Build an ensemble model to correctly classify 
+#the outcome variable and improve your model prediction by using GridSearchCV. 
+#You must apply Bagging, Boosting, Stacking, and Voting on the dataset.
+
+#Import Dataset
+df = pd.read_csv(r"C:/Users/khare/Downloads/Ensemble/Assign/Datasets_ET\\Diabeted_Ensemble.csv")
+
+# Preprocessing & EDA
+
+description = df.describe()
+Skewness = df.skew()
+Kurtosis = df.kurt()
+df.info() # No missing values
+
+df.duplicated().sum() # no duplicates
+
+# Scatter plot between the variables along with histograms
+
+sns.pairplot(df.iloc[:, :])
+
+# Input and Output Split
+predictors = df.loc[:, df.columns!=" Class variable"]
+type(predictors)
+
+target = df[" Class variable"]
+type(target)
+
+# Train Test partition of the data
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(predictors, target, test_size = 0.2, random_state=0)
+
+
+### Bagging ####
+
+from sklearn import tree
+clftree = tree.DecisionTreeClassifier()
+from sklearn.ensemble import BaggingClassifier
+
+
+bag_clf = BaggingClassifier(base_estimator = clftree, n_estimators = 500,bootstrap = True, n_jobs = 1, random_state = 42)
+
+bag_clf.fit(x_train, y_train)
+
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+# Evaluation on Testing Data
+confusion_matrix(y_test, bag_clf.predict(x_test))
+accuracy_score(y_test, bag_clf.predict(x_test))
+
+# Evaluation on Training Data
+confusion_matrix(y_train, bag_clf.predict(x_train))
+accuracy_score(y_train, bag_clf.predict(x_train))
+
+
+## Voting ###
+
+# Import the required libraries
+from sklearn import datasets, linear_model, svm, neighbors, naive_bayes
+from sklearn.ensemble import VotingClassifier
+
+# Instantiate the learners (classifiers)
+learner_1 = neighbors.KNeighborsClassifier(n_neighbors=5)
+learner_2 = linear_model.Perceptron(tol=1e-2, random_state=0)
+learner_3 = svm.SVC(gamma=0.001)
+
+# Instantiate the voting classifier
+voting = VotingClassifier([('KNN', learner_1),
+                           ('Prc', learner_2),
+                           ('SVM', learner_3)])
+
+# Fit classifier with the training data
+voting.fit(x_train, y_train)
+
+# Predict the most voted class
+hard_predictions = voting.predict(x_test)
+
+# Accuracy of hard voting
+print('Hard Voting:', accuracy_score(y_test, hard_predictions))
+
+
+
+
+
+## Soft Voting ##
+# Instantiate the learners (classifiers)
+learner_4 = neighbors.KNeighborsClassifier(n_neighbors = 5)
+learner_5 = naive_bayes.GaussianNB()
+learner_6 = svm.SVC(gamma = 0.001, probability = True)
+
+# Instantiate the voting classifier
+voting = VotingClassifier([('KNN', learner_4),
+                           ('NB', learner_5),
+                           ('SVM', learner_6)],
+                            voting = 'soft')
+
+# Fit classifier with the training data
+voting.fit(x_train, y_train)
+learner_4.fit(x_train, y_train)
+learner_5.fit(x_train, y_train)
+learner_6.fit(x_train, y_train)
+
+# Predict the most probable class
+hard_predictions = voting.predict(x_test)
+
+# Get the base learner predictions
+predictions_4 = learner_4.predict(x_test)
+predictions_5 = learner_5.predict(x_test)
+predictions_6 = learner_6.predict(x_test)
+
+# Accuracies of base learners
+print('L4:', accuracy_score(y_test, predictions_4)) 
+print('L5:', accuracy_score(y_test, predictions_5)) 
+print('L6:', accuracy_score(y_test, predictions_6)) 
+# Accuracy of Soft voting
+print('Soft Voting:', accuracy_score(y_test, hard_predictions)) 
+
+
+
+### Adaboost ###
+
+from sklearn.ensemble import AdaBoostClassifier
+
+ada_clf = AdaBoostClassifier(learning_rate = 0.02, n_estimators = 5000)
+
+ada_clf.fit(x_train, y_train)
+
+# Evaluation on Testing Data
+confusion_matrix(y_test, ada_clf.predict(x_test))
+accuracy_score(y_test, ada_clf.predict(x_test))
+
+
+
+# Evaluation on Training Data
+confusion_matrix(y_train, ada_clf.predict(x_train))
+accuracy_score(y_train, ada_clf.predict(x_train))
+
+
+
+
+## Gradiant boost ##
+
+
+from sklearn.ensemble import GradientBoostingClassifier
+import xgboost as xgb
+boost_clf = GradientBoostingClassifier()
+
+boost_clf.fit(x_train, y_train)
+
+# Evaluation on Test Data
+confusion_matrix(y_test, boost_clf.predict(x_test))
+accuracy_score(y_test, boost_clf.predict(x_test))
+
+
+
+# Evaluation on Training Data
+
+confusion_matrix(y_train, boost_clf.predict(x_train))
+accuracy_score(y_train, boost_clf.predict(x_train))
+
+
+
+
+
+## XGradiant boost ###
+
+import xgboost as xgb
+
+xgb_clf = xgb.XGBClassifier(max_depths = 5, n_estimators = 10000, learning_rate = 0.3, n_jobs = -1)
+
+xgb_clf.fit(x_train, y_train)
+
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+# Evaluation on Testing Data
+confusion_matrix(y_test, xgb_clf.predict(x_test))
+accuracy_score(y_test, xgb_clf.predict(x_test))   # 78%
+
+
+# Evaluation on Training Data
+confusion_matrix(y_train, xgb_clf.predict(x_train))
+accuracy_score(y_train, xgb_clf.predict(x_train))   # 100%
+
+xgb.plot_importance(xgb_clf)   # Plot of the xgb model
+
+
+xgb_clf = xgb.XGBClassifier(n_estimators = 500, learning_rate = 0.1, random_state = 42)
+
+param_test1 = {'max_depth': range(3,10,2), 'gamma': [0.1, 0.2, 0.3],
+               'subsample': [0.8, 0.9], 'colsample_bytree': [0.8, 0,9],
+               'rag_alpha': [1e-2, 0.1, 1]}
+
+# Grid Search
+from sklearn.model_selection import GridSearchCV
+grid_search = GridSearchCV(xgb_clf, param_test1, n_jobs = -1, cv = 5, scoring = 'accuracy')
+
+grid_search.fit(x_train, y_train)
+
+cv_xg_clf = grid_search.best_estimator_
+
+# Evaluation on Testing Data with model with hyperparameter
+accuracy_score(y_test, cv_xg_clf.predict(x_test)) 
+grid_search.best_params_
+
+
+# Evaluation on Testing Data with model with hyperparameter
+accuracy_score(y_train, cv_xg_clf.predict(x_train))
+
+
+
+
+
+## Staking ##
+# compare standalone models for binary classification
+from numpy import mean
+from numpy import std
+
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from matplotlib import pyplot
+
+X = x_train
+y = y_train
+# get a list of models to evaluate
+def get_models():
+	models = dict()
+	models['lr'] = LogisticRegression()
+	models['knn'] = KNeighborsClassifier()
+	models['cart'] = DecisionTreeClassifier()
+	models['svm'] = SVC()
+	models['bayes'] = GaussianNB()
+	return models
+
+# evaluate a given model using cross-validation
+def evaluate_model(model, X, y):
+	cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+	scores = cross_val_score(model, X, y, scoring='accuracy', cv=cv, n_jobs=-1, error_score='raise')
+	return scores
+
+# get the models to evaluate
+models = get_models()
+# evaluate the models and store results
+results, names = list(), list()
+for name, model in models.items():
+	scores = evaluate_model(model, X, y)
+	results.append(scores)
+	names.append(name)
+	print('>%s %.3f (%.3f)' % (name, mean(scores), std(scores)))
+# plot model performance for comparison
+pyplot.boxplot(results, labels=names, showmeans=True)
+pyplot.show()
+
+
+# make a prediction with a stacking ensemble
+from sklearn.ensemble import StackingClassifier
+# define dataset
+# define the base models
+level0 = list()
+level0.append(('lr', LogisticRegression()))
+level0.append(('knn', KNeighborsClassifier()))
+level0.append(('cart', DecisionTreeClassifier()))
+level0.append(('svm', SVC()))
+level0.append(('bayes', GaussianNB()))
+# define meta learner model
+level1 = LogisticRegression()
+# define the stacking ensemble
+model = StackingClassifier(estimators=level0, final_estimator=level1, cv=5)
+
+# fit the model on all available data
+model.fit(X, y)
+
+# make a prediction on test data
+yhat = model.predict(x_test)
+
+# Evaluation on Testing Data
+confusion_matrix(y_test, yhat)
+accuracy_score(y_test,yhat)
+
+
+# Evaluation on Training Data
+confusion_matrix(y_train, model.predict(x_train))
+accuracy_score(y_train, model.predict(x_train))
